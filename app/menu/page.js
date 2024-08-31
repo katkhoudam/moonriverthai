8c49cb6dfb30a120b0d469169ec642255594431c
Muhammed Katkhouda <93131462+katkhoudam@users.noreply.github.com>
2024-08-31 21:16:23 +0300

    Update page.js

--- app/menu/page.js
+++ app/menu/page.js
@@ -2,7 +2,149 @@ export default function Menu() {
        return (
          <div className="p-10">
            <h1 className="text-3xl font-bold">Our Menu</h1>
-           <p className="mt-4">Check out our delicious offerings.</p>
+           <p className="mt-4">Chef’s Recommended $18.95
+LAMB DELIGHT *
+Stewed Lamb Leg in Massaman Curry Sauce served over steamed mixed vegetables. Served with Jasmine Rice. (Contains Peanut)
+THAI DUCK CURRY **
+Stewed Duck in Red curry Sauce Served over steamed mixed vegetables. Served with Jasmine Rice.
+SEAFOOD WITH YELLOW CURRY *
+Stir Fried Seafood with Yellow Curry Powder, Egg, Half-&-Half, Onion, Bell Pepper, and celery in brown sauce. Served with Jasmine Rice.
+SEAFOOD HOT POT **
+ Stir Fried Seafood with onion, Bell Pepper,    ucchini in a spicy sauce. Served with Jasmine Rice.
+SALMON CURRY **
+Pan Fried salmon over steamed mixed vegetables, topped with red curry sauce. Served with Jasmine Rice.
+SALMON GINGER *
+Pan Fried Salmon topped with stir fried ginger, onion, bell pepper, celery, sugar pea, and mushroom in a brown sauce. Served With Jasmine Rice.
+WHOLE FISH *
+Deep Fried Whole fish over mixed vegetables topped with sweet chili sauce. Served with Jasmine Rice. Sold at Market Price.
+
+CHOICE OF PROTEIN
+(LUNCH / DINNER)
+
+VEGETABLE, TOFU, CHICKEN, PORK
+$ 10.95 / $ 14.95
+
+BEEF, SHRIMP, SQUID
+$ 12.95 / $ 16.95
+
+MOON RIVER THAI
+LUNCH MONDAY - FRIDAY
+11:30 - 2:30
+
+DINNER MONDAY - SATURDAY
+5:00 - 9:00
+
+
+
+
+
+
+
+
+EXTRA PROTEIN
+DUCK, SEAFOOD
+ $ 18.95
+
+SIDES & EXTRAS
+SAUCES $1.00
+
+VEGETABLE, TOFU $2.00 CHICKEN, PORK $2.00 BEEF, SHRIMP, SQUID $3.00 DUCK, SEAFOOD $10.00
+EGG $1.25 FRIED EGG $2.00
+
+(SUBSTITUTION / À LA CARTE)
+JASMINE RICE  $2.00 BROWN RICE $2.00 / $3.00
+EGG FRIED RICE $2.00 / $3.00 STEAMED NOODLES $3.00 STEAMED VEGETABLES $3.00
+MILD(*) MEDIUM(**)
+PEANUT SAUCE SWEET SAUCE HOISIN SAUCE CHILI PASTE SRIRACHA
+SOY SAUCE CHILI POWDER BROWN SAUCE
+
+
+
+
+
+
+THAI HOT(****)
+627 N. MAIN ST. DANVILLE, VA, 24540
+
+434-791-2939
+
+Appetizers $8.95
+CRISPY ROLLS
+(Chicken or Vegetable, With Sweet Sauce)
+FRESH ROLLS
+(Chicken, Shrimp, or Vegetable, With Peanut Sauce & Hoisin)
+CHICKEN SATAY
+(Grilled Chicken, With Peanut Sauce & Sweet cucumber Sauce)
+CALAMARI
+(Fried Squid & tempura Vegetables, With Sweet Sauce)
+POTSTICKERS
+(Meat or Vegetable, With Sweet Sauce)
+Soups & Salads
+TOM KHA *
+(SMALL $5.95, LARGE $9.95)
+Coconut milk soup, Mushroom, Onion, Tomato, Green Onion,    & Lime Juice
+TOM YUM **
+(SMALL $5.95, LARGE $9.95)
+Hot & Sour Soup, Mushroom, Onion, Tomato, & Green Onion
+WONTON SOUP *
+(SMALL $5.95, LARGE $9.95)
+Chicken, Shrimp, & Pork Wontons, Bok Choy, Fried Garlic, & Green Onion in Chicken Broth
+RICE SOUP *
+(SMALL $5.95, LARGE $9.95)
+Rice, Ginger, Fried Garlic, & Green Onion, in Chicken Broth
+SIDE SALAD $2.95
+Lettuce, Spring Mix, carrot, house Peanut Dressing
+HOUSE SALAD $9.95
+Lettuce, Spring Mix, Boiled Egg, Bell Pepper, Tomato, Carrot, Cucumber, Red Onion, House Peanut Dressing
+NAM TOK SALAD ** $14.95
+Spicy Beef Salad, lime juice, fish sauce, green onion, red onion, Ground Roast Rice, & cilantro, Stir-Fried in a Spicy Sauce. Served with Jasmine Rice
+Thai Curry
+GREEN CURRY **
+Green Curry Paste, Coconut milk, Eggplant, Bamboo Shoot, Green Bean, Bell Pepper, & Basil Leaf. Served with Jasmine Rice
+RED CURRY **
+Red Curry Paste, Coconut Milk, Eggplant, Bamboo Shoot, Green Bean, Bell Pepper, & Basil Leaf. Served with Jasmine Rice
+PANANG CURRY **
+ Panang Curry Paste, Coconut Milk, Bell Pepper, Onion, &    ucchini.
+Served with Jasmine Rice (Contains Peanut)
+MASSAMAN CURRY **
+MAssaman Curry Paste, Coconut Milk, Onion, Carrot, Peanut, & Sweet Potato. Served with Jasmine Rice
+YELLOW CURRY **
+Yellow Curry Paste, Coconut Milk, Onion, Carrot, Tomato, Sweet Potato, & Pineapple. Served with Jasmine Rice
+Stir-Fry Entrées
+BASIL **
+Onion, Bell Pepper, Bamboo Shoot, Green Bean, & Basil LEaf in a Spicy Sauce. Served with Jasmine Rice
+CRISPY GARLIC
+ Broccoli, Carrot, Onion, Celery, Bell Pepper, Mushroom,    ucchini & Fried Garlic, brown sauce. Served with Jasmine Rice
+CASHEW NUT
+Carrot, Onion, Celery, Bell Pepper, Mushroom, Pineapple, Sugar Pea, & Cashew in a brown Sauce. Served with Jasmine Rice
+GINGER *
+Fresh Ginger, Bell Pepper, Onion, Celery, Sugar Pea, & Mushroom in a brown sauce. Served with Jasmine Rice
+SWEET AND SOUR
+Tomato, Pineapple, Cucumber, Onion, & Green Onion in a Homemade Sauce. Served With Jasmine Rice
+PEPPER STEAK
+Bell Pepper, Onion, Mushroom, Sugar Pea, & Tomato in a brown Sauce. Served with Jasmine Rice
+MIXED VEGETABLE
+ Broccoli, Carrot, Onion, Bell Pepper, Celery, Mushroom,    ucchini, & BeanSprout, brown Sauce. Served with Jasmine Rice
+Noodle Entrées
+DONG BANG **
+ Udon Noodle,    ucchini, Onion, Bell Pepper in a Spicy Sauce
+PAD THAI
+Flat Rice Stick Noodle, Egg, Green Onion, & BeanSpout in a Tamarind Sauce. (with peanut, Beansprout & Lime on side)
+PAD SEE-EW
+Fresh Rice Noodle, Egg, Broccoli, Carrot, & Mushroom in a brown Sauce
+PAD WOON SEN
+Bean Thread Glass Noodle, Egg, Celery, Onion, Carrot, Sugar Pea, Green Onion, Beansprout, & Tomato in a brown Sauce
+SPICY NOODLE **
+Fresh Rice Noodle, Egg, Broccoli, Carrot, Onion, Tomato, basil Leaf, beansprout, zucchini, & Mushroom in a Spicy Sauce
+PHO NOODLE SOUP
+Fresh Thin Rice Noodle, Sweet Onion, Green Onion, Fried Garlic, & Cilantro in a Chicken broth. (with Basil Leaf, Cilantro, Beansprout, & JalapeŠo on side)
+Fried Rice Entrées
+THAI FRIED RICE
+Jasmine Rice, Egg, onion, Tomato, & Green Onion Stir-Fried in Brown Sauce
+PINEAPPLE FRIED RICE
+Jasmine Rice, Yellow Curry Powder, Egg, Onion, Tomato, Green Onion, Pineapple & Cashew Stir-Fried in Brown Sauce
+BASIL FRIED RICE **
+Jasmine Rice, Egg, Onion, Bell Pepper, Green Bean, & Basil LeAf Stir-Fried in Brown Sauce</p>
          </div>
        );
      }
