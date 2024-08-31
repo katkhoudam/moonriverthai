4e3630a514d578dc648396f47b5957a039af3c02
Muhammed Katkhouda <93131462+katkhoudam@users.noreply.github.com>
2024-08-31 20:01:11 +0300

    Update OurStory.tsx

--- public/OurStory.tsx
+++ public/OurStory.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React from 'react';
-import Image from 'next/image';
+
 
 import storyImage2 from "../public/our-story-2.jpg";
 
@@ -9,10 +9,7 @@ export const OurStory = () => {
   return (
     <div className="container mx-auto px-4 py-8">
       <div className="flex flex-wrap justify-center items-center">
-        <div className="w-full md:w-1/2 p-4" data-aos="fade-right" data-aos-duration="2000" data-aos-anchor-placement="top-bottom">
-          
-          <Image src={storyImage2} alt="Story" className="mt-4 rounded-lg shadow-lg" style={{ width: 'auto', height: 'auto' }} />
-        </div>
+       
         <div className="w-full md:w-1/2 p-4" data-aos="fade-left" data-aos-duration="2000">
           <h3 className="text-3xl font-bold">
             OUR <span className="text-blue-500">STORY</span>
