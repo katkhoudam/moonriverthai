34ab91a4f69c6a67e6e86195a6227259ab27b21f
Muhammed Katkhouda <93131462+katkhoudam@users.noreply.github.com>
2024-08-31 20:19:33 +0300

    Update OurStory.js

--- components/OurStory.js
+++ components/OurStory.js
@@ -8,10 +8,7 @@ export const OurStory = () => {
   return (
     <div className="container mx-auto px-4 py-8">
       <div className="flex flex-wrap justify-center items-center">
-        <div className="w-full md:w-1/2 p-4" data-aos="fade-right" data-aos-duration="2000" data-aos-anchor-placement="top-bottom">
-          
-          <Image src={storyImage2} alt="Story" className="mt-4 rounded-lg shadow-lg" style={{ width: 'auto', height: 'auto' }} />
-        </div>
+       
         <div className="w-full md:w-1/2 p-4" data-aos="fade-left" data-aos-duration="2000">
           <h3 className="text-3xl font-bold">
             OUR <span className="text-blue-500">STORY</span>
