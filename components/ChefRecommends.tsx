e26028dd3cb50e5bc56cd5541736d7722aaf50c1
Muhammed Katkhouda <93131462+katkhoudam@users.noreply.github.com>
2025-01-12 00:51:05 +0200

    Update ChefRecommends.tsx

--- components/ChefRecommends.tsx
+++ components/ChefRecommends.tsx
@@ -5,7 +5,7 @@ export default function ChefRecommends() {
   return (
    
    <div class="h-screen flex items-center justify-center">
-    <Image src="/chefrec.png" width={500} height={500} alt="" />
+    <Image src="/chefrec.png" width={800} height={800} alt="" />
    </div>
          )
 }
