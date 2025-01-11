7f1a5a2ef06b19afe740294cebdec1f2dece11c3
Muhammed Katkhouda <93131462+katkhoudam@users.noreply.github.com>
2025-01-12 00:47:25 +0200

    Update ChefRecommends.tsx

--- components/ChefRecommends.tsx
+++ components/ChefRecommends.tsx
@@ -2,5 +2,10 @@
 import Image from 'next/image'
  
 export default function ChefRecommends() {
-  return <Image src="/chefrec.png" width={500} height={500} alt="" />
+  return(
+   
+   <divclass="h-screen flex items-center justify-center">
+    <Image src="/chefrec.png" width={500} height={500} alt="" />
+   </div>
+         )
 }
